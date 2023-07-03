6c5783c8ba94ab9bbe9824ce61669b8abf2c950a
farmhutsoftwareteam <108745208+farmhutsoftwareteam@users.noreply.github.com>
2023-07-03 13:56:14 +0200

    search

--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 const SearchRes = require('../models/search');
+const fs = require('fs');
 const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
   apiKey: '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',
@@ -10,18 +11,63 @@ const configuration = new Configuration({
 const router = express.Router();
 
 // Function to search the database using OpenAI
+// Function to search the database using OpenAI
 async function searchDatabase(prompt, databaseType, requestId) {
   try {
+    console.log(prompt)
+    console.log(databaseType);
     const openai = new OpenAIApi(configuration);
+
+    // Retrieve the appropriate database based on the databaseType
+    let database;
+    switch (databaseType) {
+      case 'products':
+        database = await getProductDatabase();
+        break;
+      case 'machinery':
+        database = await getMachineryDatabase();
+        break;
+      case 'farm-inputs':
+        database = await getFarmInputDatabase();
+        break;
+      case 'trucks':
+        database = await getTrucksDatabase();
+        break;
+      case 'livestock':
+        database = await getLivestockDatabase();
+        break;
+      default:
+        throw new Error('Invalid database type');
+    }
+
+    // Remove _id and __v properties from each object in the database
+    const cleanedDatabase = database.map((item) => {
+      const { _id, __v, ...cleanedItem } = item;
+      return cleanedItem;
+    });
+    console.log(cleanedDatabase)
+    const systemMessage = {
+      role: 'system',
+      content: `You are a search engine that is based on the following database: ${JSON.stringify(
+        cleanedDatabase
+      )}. You will answer the following question: ${prompt}. Only return responses that directly answer the user's question.`,
+    };
+    console.dir(systemMessage.content);
+    const content = systemMessage.content;
+    
+    fs.writeFile('log.txt', content, (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log('Content logged to file successfully.');
+      }
+    });
+    
+
 
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo-16k',
-      messages: [
-        {
-          role: 'system',
-          content: `You are a search engine. You will crawl the ${databaseType} database and answer the following question: ${prompt}. Only return responses that directly answer the user's question.`,
-        },
-      ],
+      messages: [systemMessage],
     });
 
     await SearchRes.findOneAndUpdate(
@@ -34,6 +80,97 @@ async function searchDatabase(prompt, databaseType, requestId) {
   } catch (error) {
     console.error(error);
     throw new Error('Failed to search the database using OpenAI');
+  }
+}
+
+
+// Retrieve the product database
+async function getProductDatabase() {
+  try {
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://goldfish-app-d5n57.ondigitalocean.app/products/all',
+      headers: {},
+    };
+
+    const response = await axios.request(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to retrieve the product database');
+  }
+}
+
+// Retrieve the machinery database
+async function getMachineryDatabase() {
+  try {
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://goldfish-app-d5n57.ondigitalocean.app/machinery/',
+      headers: {},
+    };
+
+    const response = await axios.request(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to retrieve the machinery database');
+  }
+}
+
+// Retrieve the farm input database
+async function getFarmInputDatabase() {
+  try {
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://goldfish-app-d5n57.ondigitalocean.app/farm-inputs/all',
+      headers: {},
+    };
+
+    const response = await axios.request(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to retrieve the farm input database');
+  }
+}
+
+// Retrieve the trucks database
+async function getTrucksDatabase() {
+  try {
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://goldfish-app-d5n57.ondigitalocean.app/logistics/trucks/all',
+      headers: {},
+    };
+
+    const response = await axios.request(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to retrieve the trucks database');
+  }
+}
+
+// Retrieve the livestock database
+async function getLivestockDatabase() {
+  try {
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://goldfish-app-d5n57.ondigitalocean.app/livestock/',
+      headers: {},
+    };
+
+    const response = await axios.request(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to retrieve the livestock database');
   }
 }
 
